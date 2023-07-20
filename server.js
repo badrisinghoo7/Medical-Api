f2d8295e4930db2b510c62b2603562f9b13d24df
badrisinghoo7 <123944508+badrisinghoo7@users.noreply.github.com>
2023-07-20 20:38:42 +0530

    Update server.js
    i have update my server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// server.js
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
 server.use(router);
-server.listen(5000, () => {
-  console.log("JSON Server is running");
-});
+
+server.listen(port);
